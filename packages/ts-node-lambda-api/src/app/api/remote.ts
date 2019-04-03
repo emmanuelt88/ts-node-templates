382ccb6b9120d9206c8a8b9d0bafc3ead7603b9f
emmanuelt88 <emmanuelt88@gmail.com>
2019-04-03 13:18:07 -0300

    general fixes

--- packages/ts-node-lambda-api/src/app/api/remote.ts
+++ packages/ts-node-lambda-api/src/app/api/remote.ts
@@ -4,6 +4,7 @@ import { AppConfig, ApiLambdaApp } from "typescript-lambda-api"
 import { configureApi } from "./config";
  
 export async function handler(event, context) {
+   try {
     let appConfig = new AppConfig();
  
     appConfig.base = "/api/v1"
@@ -12,7 +13,14 @@ export async function handler(event, context) {
     let controllersPath = path.join(__dirname, "controllers")
     let app = new ApiLambdaApp(controllersPath, appConfig)
  
-    configureApi(app);
     
+    configureApi(app);
+    console.info(controllersPath);    
+    console.info(app);    
     return await app.run(event, context)
+
+   } catch (error) {
+       console.error(error);
+       return path.join(__dirname, "controllers");
+   }
 }
--- packages/ts-node-library/src/index.ts
+++ packages/ts-node-library/src/index.ts
@@ -1,11 +1,8 @@
 import { SaludatorService, ConsumerSaludatorService } from './lib/services';
-import ContainerProvider from "./lib/platform/ioc.provider";
 
-const container = ContainerProvider.getInstance();
 
 
 export {
-  container,
   SaludatorService,
   ConsumerSaludatorService
 }
